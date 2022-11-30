017ccd3502282f95413e232f55956c4d1ac60b76
karakada001 <gotokron001@gmail.com>
2022-11-30 17:18:11 +0700

    update

--- week702.php
+++ week702.php
@@ -1,10 +1,19 @@
 <?php
-    Class Calculation{
-        public $a, $b, $c; //property
+class person{
+    public $name = "Elon Mask"; $age=50;//property
 
-        function sum(){ //method
-            $this->c = $this->a + $this->b;
-        }
+    function __construct($name , $age)
+    {
+        $this->name = $n;
+        $this->age = $a;
     }
-    $c1 = new Calculation(); //instance object
+    function show (){
+        print($this->name . $this->age);
+    }
+}
+
+    $obj1 = new Person ("Mark Zuckerberg",50);
+   // $obj1->name = "Mark Zuckerberg<br />";
+   // $obj1->age = 50;
+    $obj1->show();
 ?>
